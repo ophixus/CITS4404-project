2d6b71de064831c31fc2fa7773708dd2e3420248
ophixus <74233360+ophixus@users.noreply.github.com>
2023-04-20 16:41:03 +0800

    Implement MACD and RSI
    - Populate calculate_indicators with code to calculate Moving Average Convergence Divergence (MACD) and Relative Strength Index (RSI) using the TA library
    - Implement buy and sell triggers that are activated when specific parameters for MACD and RSI are met
    - Add default parameter values and trigger conditions for the optimise_strategy placeholder

--- main.py
+++ main.py
@@ -13,38 +13,49 @@ def fetch_data(timeframe):
 class TradingBot:
     def __init__(self, data):
         self.data = data
+        self.df = None
+        self.params = {}
+        self.triggers = {}
 
     # Calculates indicators using historical data
-    # Returns a dataframe with the indicators
+    # Stores results as a dataframe with the indicators
     def calculate_indicators(self):
-        pass
+        df = pd.DataFrame(self.data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+        df['macd_diff'] = ta.trend.macd_diff(df['close'], window_slow=26, window_fast=12)
+        df['rsi'] = ta.momentum.rsi(df['close'], window=14)
+        self.df = df
 
     # Determines if a buy signal should be triggered
     # Returns True if parameters are met, False otherwise
-    def buy_trigger(self):
-        pass
+    def buy_trigger(self, t):
+        macd_diff = self.df['macd_diff'][t] > self.params['buy_macd_diff']
+        rsi = self.df['rsi'][t] < self.params['buy_rsi']
+        return macd_diff and rsi
 
     # Determines if a sell signal should be triggered
     # Returns True if parameters are met, False otherwise
-    def sell_trigger(self):
-        pass
+    def sell_trigger(self, t):
+        macd_diff = self.df['macd_diff'][t] < self.params['sell_macd_diff']
+        rsi = self.df['rsi'][t] > self.params['sell_rsi']
+        return macd_diff and rsi
 
     # Optimises the trading strategy using adaptive AI techniques
-    # Returns the optimised parameters and triggers
+    # Stores the optimised parameters and triggers as class attributes
     def optimise_strategy(self):
-        pass
+        self.params.append({'buy_macd_diff': 0.1, 'buy_rsi': 30, 'sell_macd_diff': -0.1, 'sell_rsi': 70})
+        self.triggers = {'buy': self.buy_trigger, 'sell': self.sell_trigger}
 
     # Simulates trades using historical data
     # Returns performance metrics
-    def evaluate_performance(self, params, triggers):
+    def evaluate_performance(self):
         pass
 
     # Runs the trading strategy
     # Prints performance metrics
     def run(self):
-        df = self.calculate_indicators()
-        params, triggers = self.optimise_strategy()
-        performance = self.evaluate_performance(params, triggers)
+        self.calculate_indicators()
+        self.optimise_strategy()
+        performance = self.evaluate_performance()
         for key, value in performance.items():
             print(key, value)
 
