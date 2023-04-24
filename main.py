014a66d65bbd90ae2be7be92e6f04036707374ca
ophixus <74233360+ophixus@users.noreply.github.com>
2023-04-24 23:52:42 +0800

    Refactor code
    - Move the TradingBot class into a new file for better organisation
    - Move calculate_indicators and parameters out of the TradingBot class to enhance modularity
    - Replace optimise_strategy with a function that allows different trigger expressions to be set for each TradingBot instance
    - Add a generate_triggers function that returns a buy/sell trigger based on the input expression
    - Remove obsolete sell_trigger and buy_trigger functions
    - Add a new performance metric (win rate)
    - Add support for interchangeable optimisation modules to be used in the future

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 import ccxt
 import ta
 import pandas as pd
-
-# Constants
-START_BALANCE = 100
-FEES = 0.02
+from trading_bot import TradingBot
+# from optimisation_[STRATEGY NAME] import optimise_triggers
 
 # Fetch historical data from Kraken exchange
 # Returns OHLCV data as a list of lists
@@ -13,102 +11,22 @@ def fetch_data(timeframe):
     ohlcv = exchange.fetch_ohlcv('BTC/AUD', timeframe=timeframe)
     return ohlcv
 
-# Trading bot class that will be used to run the trading strategy
-class TradingBot:
-    def __init__(self, data):
-        self.data = data
-        self.df = None
-        self.params = {}
-        self.triggers = {}
-        self.balance = START_BALANCE
-        self.holdings = 0
-        self.trades = None
-
-    # Calculates indicators using historical data
-    # Stores results as a dataframe with the indicators
-    def calculate_indicators(self):
-        self.df = pd.DataFrame(self.data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
-        self.df['macd_diff'] = ta.trend.macd_diff(self.df['close'], window_slow=26, window_fast=12)
-        self.df['rsi'] = ta.momentum.rsi(self.df['close'], window=14)
-
-    # Determines if a buy signal should be triggered
-    # Returns True if parameters are met, False otherwise
-    def buy_trigger(self, t):
-        macd_diff = self.df['macd_diff'][t] > self.params['buy_macd_diff']
-        rsi = self.df['rsi'][t] < self.params['buy_rsi']
-        return macd_diff and rsi
-
-    # Determines if a sell signal should be triggered
-    # Returns True if parameters are met, False otherwise
-    def sell_trigger(self, t):
-        macd_diff = self.df['macd_diff'][t] < self.params['sell_macd_diff']
-        rsi = self.df['rsi'][t] > self.params['sell_rsi']
-        return macd_diff and rsi
-
-    # Optimises the trading strategy using adaptive AI techniques
-    # Stores the optimised parameters and triggers as class attributes
-    def optimise_strategy(self):
-        self.params.update({'buy_macd_diff': 0.1, 'buy_rsi': 30, 'sell_macd_diff': -0.1, 'sell_rsi': 70})
-        self.triggers = {'buy': self.buy_trigger, 'sell': self.sell_trigger}
-
-    # Spends all available balance on a trade
-    # Adds a new trade to the trades dataframe
-    def buy(self, t, price):
-        self.holdings = self.balance / price * (1 - FEES)
-        self.balance = 0
-        self.trades.loc[len(self.trades)] = [t, price, None, None, None]
-
-    # Sells all available holdings on a trade
-    # Updates the trade in the trades dataframe
-    def sell(self, t, price):
-        self.balance = self.holdings * price * (1 - FEES)
-        self.holdings = 0
-        self.trades.loc[len(self.trades) - 1][2:4] = [t, price]
-
-    # Simulates trades using historical data
-    # Stores the trades in a dataframe
-    def simulate_trades(self):
-        # Create empty dataframe to store trades
-        self.trades = pd.DataFrame(columns=['entry_time', 'entry_price', 'exit_time', 'exit_price', 'profit'])
-
-        # Iterate through all time steps
-        for t in range(len(self.df)):
-            # Check if buy or sell triggers are met
-            if self.triggers['buy'](t) and not self.holdings:
-                self.buy(t, self.df['close'][t])
-            elif self.triggers['sell'](t) and not self.balance:
-                self.sell(t, self.df['close'][t])
-        
-        # At the end of the simulation, sell all holdings and calculate profit per trade
-        if self.holdings:
-            self.sell(t, self.df['close'][t])
-        self.trades['profit'] = self.trades['exit_price'] - self.trades['entry_price']
-
-    # Evaluates the performance of the trading strategy
-    # Returns a dictionary of performance metrics
-    def calculate_performance(self):
-        performance_metrics = {
-            'net_profit': "{:.2f} AUD".format(self.balance - START_BALANCE),
-            'total_trades': len(self.trades),
-            'average_profit': "{:.2f} AUD".format(self.trades['profit'].mean())
-        }
-        return performance_metrics
-
-    # Runs the trading strategy
-    # Prints performance metrics
-    def run(self):
-        self.calculate_indicators()
-        self.optimise_strategy()
-        self.simulate_trades()
-        performance = self.calculate_performance()
-        for key, value in performance.items():
-            print(key, value)
+# Calculates indicators using historical data
+# Returns results as a dataframe with the indicators
+def calculate_indicators(data):
+    df = pd.DataFrame(data, columns=['timestamp', 'open', 'high', 'low', 'close', 'volume'])
+    df['macd_diff'] = ta.trend.macd_diff(df['close'], window_slow=26, window_fast=12)
+    df['rsi'] = ta.momentum.rsi(df['close'], window=14)
+    return df
 
 # Main function
 def main():
     data = fetch_data('1m')
-    bot = TradingBot(data)
-    bot.run()
+    df = calculate_indicators(data)
+    expressions = [] # Will eventually be replaced by optimise_triggers(df)
+    bot = TradingBot(df)
+    bot.set_trigger_expressions(expressions)
+    bot.run(True)
 
 # Run main function
 if __name__ == "__main__":
